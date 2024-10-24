10b181fe930ae192bbf7b21358f6d0d4803dd7a2
VGJO1990 <manloprue@alum.us.es>
2024-10-24 17:42:13 +0200

    Primeras tres funciones a falta de typing

--- src/poblacion.py
+++ src/poblacion.py
@@ -1,8 +1,15 @@
 from collections import namedtuple
 import csv
 RegistroPoblacion = namedtuple('RegistroPoblacion', 'pais, codigo, año, censo')
-def lee_poblaciones(ruta_poblaciones):
-    lista_registro_poblaciones= []
+def lee_poblaciones(ruta_poblaciones:str)->list[RegistroPoblacion]:
+    """Pasar los datos del archivo csv a una lista de tuplas
+
+    :param ruta_poblaciones: Ruta del archivo csv con los datos de poblacion
+    :type ruta_poblaciones: str
+    :return:    Lista de tuplas con los datos de poblacion
+    :rtype: list[RegistroPoblacion]
+    """
+    lista_registro_poblaciones= set()
     with open(ruta_poblaciones,encoding='utf-8') as f:
         lectorcsv = csv.reader(f)
         for l in lectorcsv:
@@ -11,17 +18,16 @@ def lee_poblaciones(ruta_poblaciones):
             año = int(l[2])
             censo = int(l[3])
             regPoblacion = RegistroPoblacion(pais,codigo,año,censo)
-            lista_registro_poblaciones.append(regPoblacion)
+            lista_registro_poblaciones.add(regPoblacion)
     return lista_registro_poblaciones
 
 def calcula_paises(poblaciones):
     poblaciones = lee_poblaciones(poblaciones)
-    paises=list()
+    paises= set()
     for f in poblaciones:
         pais= f[0]
-        paises.append(pais)
+        paises.add(pais)
     return sorted(paises)     
-
 
 
 def filtra_por_pais(poblaciones,nombre_o_codigo):
@@ -32,3 +38,10 @@ def filtra_por_pais(poblaciones,nombre_o_codigo):
             datos_pais.append([f[2],f[3]])
     return datos_pais
 
+def filtra_por_paises_y_anyo(poblaciones, anyo, paises):
+    datos_pais=list()
+    for f in poblaciones:
+        if f[0] == paises and f[2] == (anyo - 1):
+            datos_pais.append([f[0],f[3]])
+            
+    return datos_pais
--- src/poblacion_test.py
+++ src/poblacion_test.py
@@ -1,12 +1,20 @@
 from poblacion import *
 def test_lee_poblaciones(lista_poblaciones):
      print("Lista de poblaciones")
-     print(lista_poblaciones)
+     print(f"Se han censado {len(lista_poblaciones)}")
+     lista = list(lista_poblaciones)
+     print(f"Las dos primeras son{lista[:2]}")
+     print(f"Las dos ultimas  son{lista[-2:]}")
 def test_calcula_paises(poblaciones):
-     print('Los paises censados son: ')
-     print(poblaciones)
+     print(f'Se han censado {len(poblaciones)}')
+     lista = list(poblaciones)
+     print(f"Las dos primeras son{lista[:2]}")
+     print(f"Las dos ultimas  son{lista[-2:]}")
 def test_filtra_por_pais(poblaciones):
      print("Los datos del pais elegido son: ")
+     print(poblaciones)
+def test_filtra_por_paises_y_anyo(poblaciones):
+     print("Los datos del pais elegido para el año anterior al elegido son: ")
      print(poblaciones)
 
 
@@ -14,7 +22,7 @@ if __name__ == '__main__':
     var ='data/population.csv'
     #print(test_lee_poblaciones(lee_poblaciones(var)))
     #print(test_calcula_paises(calcula_paises(var)))
-    #print(test_filtra_por_pais(filtra_por_pais(var,"Panama")))
+    print(test_filtra_por_pais(filtra_por_pais(var,"Panama")))
     #print (test_filtra_por_pais(filtra_por_pais(var,"PAN"))) #Deberia salir lo mismo que en el de arriba
-
+    #print(test_filtra_por_paises_y_anyo(filtra_por_paises_y_anyo(var,1982,"Panama")))
     
