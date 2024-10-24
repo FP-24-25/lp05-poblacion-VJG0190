d28e49e90ee695816b81c803d2d6ffb62532d595
VGJO1990 <manloprue@alum.us.es>
2024-10-24 10:10:18 +0200

    Dos primeras funciones

--- src/poblacion.py
+++ src/poblacion.py
@@ -3,13 +3,22 @@ import csv
 RegistroPoblacion = namedtuple('RegistroPoblacion', 'pais, codigo, año, censo')
 def lee_poblaciones(ruta_poblaciones):
     lista_registro_poblaciones= []
-    with open(ruta_poblaciones,encoding='utf-8'):
-        lectorcsv = csv.reader(ruta_poblaciones)
+    with open(ruta_poblaciones,encoding='utf-8') as f:
+        lectorcsv = csv.reader(f)
         for l in lectorcsv:
-            pais= l[0]
+            pais= l[0].strip() #En el documento csv, en el primer elemento hay veces que hay espacios que no dejan funcionar al codigo de normal.
             codigo = l[1]
             año = int(l[2])
             censo = int(l[3])
             regPoblacion = RegistroPoblacion(pais,codigo,año,censo)
             lista_registro_poblaciones.append(regPoblacion)
     return lista_registro_poblaciones
+
+def calcula_paises(poblaciones):
+    poblaciones = lee_poblaciones(poblaciones)
+    paises=list()
+    for f in poblaciones:
+        pais= f[0]
+        paises.append(pais)
+    return sorted(paises)     
+
--- src/poblacion_test.py
+++ src/poblacion_test.py
@@ -1,11 +1,17 @@
 from poblacion import *
-def test_lee_poblaciones(ruta_poblaciones):
-    lista_poblaciones =lee_poblaciones(ruta_poblaciones)
-    print("Lista de poblaciones")
-    print(lista_poblaciones)
+def test_lee_poblaciones(lista_poblaciones):
+     print("Lista de poblaciones")
+     print(lista_poblaciones)
+def test_calcula_paises(poblaciones):
+     print('Los paises censados son: ')
+     print(poblaciones)
 
 
 
 if __name__ == '__main__':
-    print(test_lee_poblaciones('data/population.csv'))
+    var ='data/population.csv'
+    #print(test_lee_poblaciones(lee_poblaciones(var)))
+    print(test_calcula_paises(calcula_paises(var)))
+     
+
     
