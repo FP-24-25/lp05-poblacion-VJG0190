074d9622e22bf896f6815273e5120a7b53e1f7d0
VGJO1990 <manloprue@alum.us.es>
2024-11-13 19:43:34 +0100

    Ultima funcion(problemas en las graficas)

--- src/poblacion.py
+++ src/poblacion.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+import matplotlib.pyplot as plt
+from random import random
 import csv
 RegistroPoblacion = namedtuple('RegistroPoblacion', 'pais, codigo, año, censo')
 def lee_poblaciones(ruta_poblaciones:str)->list[RegistroPoblacion]:
@@ -34,14 +36,55 @@ def filtra_por_pais(poblaciones,nombre_o_codigo):
     poblaciones = lee_poblaciones(poblaciones)
     datos_pais=list()
     for f in poblaciones:
-        if f[0] == nombre_o_codigo or f[1] == nombre_o_codigo:
+        if f[0]  or f[1] == nombre_o_codigo:
             datos_pais.append([f[2],f[3]])
     return datos_pais
 
 def filtra_por_paises_y_anyo(poblaciones, anyo, paises):
+    poblaciones = lee_poblaciones(poblaciones)
     datos_pais=list()
     for f in poblaciones:
         if f[0] == paises and f[2] == (anyo - 1):
             datos_pais.append([f[0],f[3]])
             
     return datos_pais
+
+def muestra_evolucion_poblacion(poblaciones,nombre_o_codigo):
+    poblaciones = lee_poblaciones(poblaciones)
+    lista_años = list()
+    lista_habitantes=list()
+    for f in poblaciones:
+        if  f[0] == nombre_o_codigo or f[1] == nombre_o_codigo:
+            lista_años.append(f[2])
+            lista_habitantes.append(f[3])
+    if lista_años and lista_habitantes:
+        plt.title(f"Evolución de la población: {nombre_o_codigo}")
+        plt.plot(lista_años, lista_habitantes,)
+        plt.show()
+    else:
+        print(f"No se encontraron datos para {nombre_o_codigo}.")        
+           
+def aux_lista_paises_azar():
+    poblaciones = lee_poblaciones(poblaciones)
+    paises=calcula_paises(poblaciones)
+    return   random.sample(paises, 5)
+
+def muestra_comparativa_paises_anyo(poblaciones,paises,anyo):
+    poblaciones = lee_poblaciones(poblaciones)
+    paises= sorted(set(paises))
+    poblacion_por_pais = dict()
+    #Voy a crear un diccionario para que el pais y los habitantes vayan en el mismo orden y les doy el valor 0 antes de añadir los habitantes desde poblaciones
+    for pais in paises:                 
+        poblacion_por_pais[pais] = 0 
+    for f in poblaciones:
+        if f[2] == anyo and f[0] in poblacion_por_pais:
+            poblacion_por_pais[f[0]] = f[3]
+    lista_habitantes = []
+    for pais in paises:
+        lista_habitantes.append(poblacion_por_pais[pais])
+    
+    plt.title("Grafica")
+    plt.bar(paises, lista_habitantes)
+    plt.show() 
+
+            
--- src/poblacion_test.py
+++ src/poblacion_test.py
@@ -16,13 +16,19 @@ def test_filtra_por_pais(poblaciones):
 def test_filtra_por_paises_y_anyo(poblaciones):
      print("Los datos del pais elegido para el año anterior al elegido son: ")
      print(poblaciones)
+def test_muestra_evolucion_poblacion(poblaciones,nombre_o_codigo):
+     muestra_evolucion_poblacion(poblaciones,nombre_o_codigo)
+def test_muestra_comparativa_paises_anyo(poblaciones,anyo,paises):
+   muestra_comparativa_paises_anyo(poblaciones,anyo,paises)
 
 
 if __name__ == '__main__':
     var ='data/population.csv'
     #print(test_lee_poblaciones(lee_poblaciones(var)))
     #print(test_calcula_paises(calcula_paises(var)))
-    print(test_filtra_por_pais(filtra_por_pais(var,"Panama")))
+    #print(test_filtra_por_pais(filtra_por_pais(var,"Panama")))
     #print (test_filtra_por_pais(filtra_por_pais(var,"PAN"))) #Deberia salir lo mismo que en el de arriba
     #print(test_filtra_por_paises_y_anyo(filtra_por_paises_y_anyo(var,1982,"Panama")))
+    #test_muestra_evolucion_poblacion(var,"Panama")
+    test_muestra_comparativa_paises_anyo(var,("Panama","Puerto Rico","Costa Rica"),'1982')
     
